1b2254c8bcfb135350f34b518e359a7771f819fe
chenwu <chenwu@meitunmama.com>
2020-10-22 11:33:18 +0800

    修复json发送

--- src/main/java/com/babytree/producer/Pub_live_realtimeSingleRowProducerTest.java
+++ src/main/java/com/babytree/producer/Pub_live_realtimeSingleRowProducerTest.java
@@ -1,6 +1,5 @@
 package com.babytree.producer;
 
-import com.google.gson.Gson;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -15,7 +14,7 @@ import java.util.Properties;
 public class Pub_live_realtimeSingleRowProducerTest {
 
     public static void main(String[] args) {
-        String content = args[0];
+        String body = "{\"appointment_end_ts\":1603274580000000,\"appointment_start_ts\":1603274580000,\"business\":\"pregnancy\",\"comment_total\":0,\"comment_total_actual\":0,\"comment_total_base\":0,\"comment_user_actual\":0,\"comment_user_base\":0,\"comment_user_total\":0,\"discussion_id\":94754262,\"distribute_ts\":1603274580000,\"during\":1603277440000,\"end_ts\":1603277440000,\"follower_total\":0,\"follower_total_actual\":0,\"follower_total_base\":0,\"item_type\":1,\"live_status\":3,\"name\":\"爱动漫的初冬的雪\",\"online_max_total\":3,\"online_max_total_actual\":3,\"online_max_total_base\":0,\"owner_id\":\"u12400672386\",\"owner_type\":1,\"praise_total\":0,\"praise_total_actual\":0,\"praise_total_base\":0,\"pv_total\":6,\"pv_total_actual\":6,\"pv_total_base\":0,\"question_total\":0,\"question_total_actual\":0,\"question_total_base\":0,\"scence_id\":6326,\"start_ts\":0,\"user_type\":0,\"uv_total\":4,\"uv_total_actual\":4,\"uv_total_base\":0,\"video_type\":6}";
         String topicName = "pub_live_distribute";
         String bootStrapServerConfig = "172.26.6.87:9095,172.26.6.88:9095,172.26.6.89:9095";
         Properties properties = new Properties();
@@ -24,7 +23,7 @@ public class Pub_live_realtimeSingleRowProducerTest {
         //properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"172.26.34.117:9095,172.26.34.118:9095,172.26.34.119:9095");
         properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,bootStrapServerConfig);
         KafkaProducer producer = new KafkaProducer(properties);
-        producer.send(new ProducerRecord(topicName,content));
+        producer.send(new ProducerRecord(topicName,body));
         producer.close();
     }
 }
